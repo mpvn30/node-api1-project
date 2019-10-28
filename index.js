ff314d5074b1b08e4a634fb39c8534d01f064aa8
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-28 15:35:48 -0400

    GET completed

--- index.js
+++ index.js
@@ -8,6 +8,23 @@ const server = express();
 server.use(express.json());
 
 
+server.get('/', (req, res) => {
+    res.send('Hello')
+})
 
-const port = 8000;
-server.listen(port, () => console.log('Server is listening on port 8000'))
+//GET
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => {
+        if (users) {
+        res.status(200).json(users)
+    } else {
+        res.status(404).json({error: "Please provide name and bio for the user."});
+    }
+    })
+    .catch(error => 
+        res.status(500).send(error))
+})
+
+const port = 9000;
+server.listen(port, () => console.log('Server is listening on port 9000'))
