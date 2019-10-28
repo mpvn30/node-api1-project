80b73c495496a8ba953fdd21a398a490a8d9004a
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-28 15:03:10 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // implement your API here
+const express = require('express');
+
+const db = require('./data/db.js');
+
+const server = express();
+
+server.use(express.json());
+
+
+
+const port = 8000;
+server.listen(port, () => console.log('Server is listening on port 8000'))
